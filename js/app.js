20431070a11c79eeca8bb6a9d30f20f0f88ac0ee
Michael Enger <michael.enger@webtraffic.no>
2013-10-23 12:50:23 +0200

    Remove jQuery and jQuery-specific code

--- js/app.js
+++ js/app.js
@@ -1,43 +1,56 @@
-(function($) {
-"use strict";
+(function() {
+	"use strict";
+	window.onload = function() {
 
-// Overkill? Yes. Spam? No.
-var houseonthehill = 'thelonelycoder.com',
-	banana = document.getElementById('banana'),
-	cmd = 'otliam';
-banana.innerHTML = '<a href="' + cmd.split('').reverse().join('') + ':mike' + '&#64;' + houseonthehill + '">mike' + '&#64;' + houseonthehill + '</a>';
+		// Overkill? Yes. Spam? No.
+		var houseonthehill = 'thelonelycoder.com',
+			banana = document.getElementById('banana'),
+			cmd = 'otliam';
+		banana.innerHTML = '<a href="' + cmd.split('').reverse().join('') + ':mike' + '&#64;' + houseonthehill + '">mike' + '&#64;' + houseonthehill + '</a>';
 
-// Load projects from GitHub
-$(document).ready(function() {
-	var projects = $('#github-projects .container'),
-		blacklist = [
-			'NO-HTML5', 'Giant-Bomb-Recommengine', 'spotify-flickrstream', 'rpgdicebag'
-		];
-	$.get('https://api.github.com/users/michaelenger/repos?sort=updated', function(data) {
-		var count = 0,
-			row = '';
-		for (var i = 0; i < data.length; i++) {
-			if (blacklist.indexOf(data[i].name) != -1 || data[i].fork || !data[i].description) continue;
+		// Load projects from GitHub
+		var section = document.getElementById('other-projects'),
+			projects = document.getElementById('github-projects'),
+			request = new XMLHttpRequest(),
+			blacklist = [
+				'NO-HTML5', 'Giant-Bomb-Recommengine', 'spotify-flickrstream', 'rpgdicebag'
+			];
 
-			if (count != 0 && count % 4 == 0) {
-				projects.append('<div class="row">' + row + '</div>');
-				row = '';
+		request.addEventListener('load', function() {
+			if (this.readyState == 4 && this.status == 200 ) {
+				var data = JSON.parse(this.responseText),
+					html = '',
+					count = 0;
+
+				html = '<div class="row">';
+				for (var i = 0; i < data.length; i++) {
+					if (blacklist.indexOf(data[i].name) != -1 || data[i].fork || !data[i].description) continue;
+
+					if (count != 0 && count % 4 == 0) {
+						html += '</div><div class="row">';
+					}
+					count++;
+
+					html += '<div class="col-sm-3 project">\
+						<h3><a href="' + data[i].html_url + '">' + data[i].name + '</a> <small>' + data[i].language + '</small></h3>\
+						<p>' + data[i].description + '</p>\
+					</div>';
+				}
+				html += '</div>';
+
+				if (html.length) {
+					projects.innerHTML = html;
+				}
+
+				document.getElementById('github-loading').remove();
 			}
+		}, false);
+		request.addEventListener("error", function() {
+			console.error(this.responseText);
+			section.remove(); // let's just pretend this never happened, shall we?
+		}, false);
+		request.open('GET', 'https://api.github.com/users/michaelenger/repos?sort=updated', true);
+		request.send();
 
-			row += '<div class="col-sm-3">\
-				<h3><a href="' + data[i].html_url + '">' + data[i].name + '</a> <small>' + data[i].language + '</small></h3>\
-				<p>' + data[i].description + '</p>\
-				</div>';
-
-			count++;
-		}
-
-		if (row.length) {
-			projects.append('<div class="row">' + row + '</div>');
-		}
-
-		$('#projects-loading').remove();
-	});
-});
-
-})(jQuery.noConflict());
+	}
+})();
